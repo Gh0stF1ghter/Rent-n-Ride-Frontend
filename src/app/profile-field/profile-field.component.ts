f41846c3283a3d342fc37e9208a0de81ec99ab16
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-07-23 17:11:45 +0300

    feat: create profile field component

---
+++ src/app/profile-field/profile-field.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-field',
+  standalone: true,
+  imports: [],
+  templateUrl: './profile-field.component.html',
+  styleUrl: './profile-field.component.css'
+})
+export class ProfileFieldComponent {
+
+}
