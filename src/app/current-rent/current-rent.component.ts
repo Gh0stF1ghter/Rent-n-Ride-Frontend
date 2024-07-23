8a743f4532b4586befdc843b2c504356365eb28a
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-07-23 17:12:41 +0300

    feat: add current rent/logo component

---
+++ src/app/current-rent/current-rent.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-current-rent',
+  standalone: true,
+  imports: [],
+  templateUrl: './current-rent.component.html',
+  styleUrl: './current-rent.component.css'
+})
+export class CurrentRentComponent {
+
+}
