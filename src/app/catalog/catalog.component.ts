d59a471210d4bc4dc3391dd5cd74da4da97db22f
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-07-25 22:05:35 +0300

    feat: add paginator

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
-import { FilterListComponent } from "../filter-list/filter-list.component";
-import { SortFieldComponent } from "../sort-field/sort-field.component";
-import { CarBoxComponent } from "../car-box/car-box.component";
+import { FilterListComponent } from '../filter-list/filter-list.component';
+import { SortFieldComponent } from '../sort-field/sort-field.component';
+import { CarBoxComponent } from '../car-box/car-box.component';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-catalog',
   standalone: true,
-  imports: [FilterListComponent, SortFieldComponent, CarBoxComponent],
+  imports: [
+    FilterListComponent,
+    SortFieldComponent,
+    CarBoxComponent,
+    MatPaginatorModule,
+  ],
   templateUrl: './catalog.component.html',
-  styleUrl: './catalog.component.scss'
+  styleUrl: './catalog.component.scss',
 })
-export class CatalogComponent {
-
-}
+export class CatalogComponent {}
