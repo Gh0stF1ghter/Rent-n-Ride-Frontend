fb1f2285f26e2e61fccc451df1b3c693d8fcccfb
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-07-25 17:41:53 +0300

    feat: add car box component

---
+++ src/app/car-box/car-box.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-car-box',
+  standalone: true,
+  imports: [],
+  templateUrl: './car-box.component.html',
+  styleUrl: './car-box.component.scss'
+})
+export class CarBoxComponent {
+
+}
