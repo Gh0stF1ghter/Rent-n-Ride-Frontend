f8d04034424acd57a5892b7c883ec8721574f1bb
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-07-25 12:05:40 +0300

    feat: create searchbar

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatRippleModule } from '@angular/material/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import {
@@ -13,14 +15,17 @@ import {
   selector: 'app-search-bar',
   standalone: true,
   imports: [
+    CommonModule,
     MatInputModule,
     FormsModule,
     MatSuffix,
     MatIconModule,
     MatFormFieldModule,
+    MatRippleModule
   ],
   templateUrl: './search-bar.component.html',
-  styleUrl: './search-bar.component.css',
+  styleUrl: './search-bar.component.scss',
+  encapsulation: ViewEncapsulation.None
 })
 export class SearchBarComponent {
   value = '';
