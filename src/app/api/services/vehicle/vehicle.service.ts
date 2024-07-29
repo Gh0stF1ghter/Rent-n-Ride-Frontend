660863971c6b23c70a3ba50796618286719aa135
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-07-29 14:26:25 +0300

    feat: pass pagination to backend

--- src/app/api/services/vehicle/vehicle.service.ts
+++ src/app/api/services/vehicle/vehicle.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import pagination from '../../models/pagination';
+import { paginationKeys } from '../../constants/paramKeys';
+import vehicleModel from '../../models/apiModels/vehicleModel';
+import { catalogServiceEndpoints } from '../../constants/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VehicleService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getAll(pageParams: pagination) {
+    let params = new HttpParams()
+      .set(paginationKeys.page, pageParams.currentPage)
+      .set(paginationKeys.pageSize, pageParams.pageSize);
+
+      return this.http.get<vehicleModel[]>(catalogServiceEndpoints.vehicles, {
+        observe: 'response',
+        headers: { Accept: 'application/json' },
+        params: params
+      })
+  }
+
+  getById(id: string) {
+    return this.http.get<vehicleModel>(`${catalogServiceEndpoints.vehicles}/${id}`)
+  }
 }
