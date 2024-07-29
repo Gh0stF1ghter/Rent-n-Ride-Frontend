ca4a790fc0b8fb9879a9a956029ff106541b4a61
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-07-29 18:31:01 +0300

    feat: add filter

--- src/app/filter-list/filter-list.component.ts
+++ src/app/filter-list/filter-list.component.ts
@@ -1,11 +1,28 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { FormControl, FormsModule } from '@angular/forms';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+  viewChild,
+} from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatChipGrid, MatChipsModule } from '@angular/material/chips';
+import { MatChipsModule } from '@angular/material/chips';
 import { MatRippleModule } from '@angular/material/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import manufacturerModel from '../api/models/apiModels/manufacturerModel';
+import { ManufacturerService } from '../api/services/manufacturer/manufacturer.service';
+import pagination from '../api/models/pagination';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { Observable, startWith } from 'rxjs';
+import { map, startsWith } from 'lodash';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import carModel from '../api/models/apiModels/carModel';
+import { CarModelService } from '../api/services/car-model/car-model.service';
 
 @Component({
   selector: 'app-filter-list',
@@ -17,12 +34,66 @@ import { MatInputModule } from '@angular/material/input';
     MatAutocompleteModule,
     MatChipsModule,
     MatCheckboxModule,
-    MatRippleModule
+    MatRippleModule,
+    ReactiveFormsModule,
+    AsyncPipe,
   ],
   templateUrl: './filter-list.component.html',
   styleUrl: './filter-list.component.scss',
 })
-export class FilterListComponent {
+export class FilterListComponent implements OnInit {
   @Output() onFilterUpdated = new EventEmitter<any>();
+
+  formGroup = new FormGroup({
+    manufacturerControl: new FormControl(),
+    carModelControl: new FormControl()
+  })
+
+  manufacturers: manufacturerModel[] = [];
+  selectedManufacturer: manufacturerModel | undefined;
+
+
+  carModels: carModel[] = [];
+  selectedCarModel: carModel | undefined;
+
+  constructor(private manufacturerService: ManufacturerService, private carModelService: CarModelService) {}
+
+  ngOnInit(): void {
+    this.getManufacturers();
+  }
+
+  getManufacturers() {
+    let pagination: pagination = {
+      pageSize: 100,
+      currentPage: 1,
+    };
+
+    this.manufacturerService.getAll(pagination).subscribe((response) => {
+      if (response.body) {
+        this.manufacturers = response.body;
+      }
+    });
+  }
+
+  getManufacturer(id:string) {
+      this.manufacturerService
+        .getById(id)
+        .subscribe(
+          manufacturer => {
+            this.selectedManufacturer = manufacturer
+            console.log(manufacturer);
+            
+            this.carModels = manufacturer.carModels
+          }
+        );
+  }
+
+  getCarModel() {
+    let carModelInForm: carModel = this.formGroup.controls.carModelControl.value
+
+    if(carModelInForm) {
+      this.carModelService.getById(carModelInForm.id).subscribe(carModel => this.selectedCarModel = carModel)
+    }
+  }
   //Implement logstash debouncer
 }
