bb93e0c14941f457b9033d49025b5adca367c55e
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-07-31 15:17:48 +0300

    feat: add auth0

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,21 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
+import { provideAuth0 } from '@auth0/auth0-angular';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideHttpClient()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient(),
+    provideAuth0({
+      domain: environment.AUTH0_DOMAIN,
+      clientId: environment.AUTH0_CLIENTID,
+      authorizationParams: {
+        redirect_uri: window.location.origin,
+      },
+    }),
+  ],
 };
