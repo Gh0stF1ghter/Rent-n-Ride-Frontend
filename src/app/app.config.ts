d77dd9ac7257d56f7aaab5edbffa1c502952352c
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-08-09 15:09:23 +0300

    feat: add auth0 login logout

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,21 +3,35 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient } from '@angular/common/http';
-import { provideAuth0 } from '@auth0/auth0-angular';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import {
+  AuthHttpInterceptor,
+  authHttpInterceptorFn,
+  provideAuth0,
+} from '@auth0/auth0-angular';
 import { environment } from '../environments/environment';
+import { AdminPanelServiceEndpoints } from './api/constants/environment';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    AuthHttpInterceptor,
+    provideNativeDateAdapter(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([authHttpInterceptorFn])),
     provideAuth0({
       domain: environment.AUTH0_DOMAIN,
       clientId: environment.AUTH0_CLIENTID,
       authorizationParams: {
+        audience: 'https://rent-ride/',
         redirect_uri: window.location.origin,
+      },
+      httpInterceptor: {
+        allowedList: [
+          `${AdminPanelServiceEndpoints.general}/*`,
+        ],
       },
     }),
   ],
--- src/app/login-button/login-button.component.ts
+++ src/app/login-button/login-button.component.ts
@@ -1,17 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Comment } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
 import { MatRippleModule } from '@angular/material/core';
 import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-login-button',
   standalone: true,
-  imports: [MatRippleModule],
+  imports: [MatRippleModule, CommonModule],
   templateUrl: './login-button.component.html',
-  styleUrl: './login-button.component.scss'
+  styleUrl: './login-button.component.scss',
 })
-export class LoginButtonComponent {
-  /**
-   *
-   */
-  constructor(public auth:AuthService) {}
+export class LoginButtonComponent implements OnInit {
+  constructor(public auth: AuthService) {}
+
+  username: string | undefined;
+  email: string | undefined;
+  pictureSrc: string | undefined;
+
+  ngOnInit(): void {
+    if (this.auth.user$) {
+      this.auth.user$.subscribe((user) => {
+        this.username = user?.name;
+        this.email = user?.email;
+        this.pictureSrc = user?.picture;
+      });
+    }
+  }
 }
