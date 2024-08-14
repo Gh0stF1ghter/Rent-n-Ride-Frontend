aff62787bf3deeb640045525ce71f2f36a78ad50
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-08-14 18:01:18 +0300

    Update dialog-user.component.ts

--- src/app/dialog-user/dialog-user.component.ts
+++ src/app/dialog-user/dialog-user.component.ts
@@ -43,4 +43,70 @@ import updateUserApiModel from '../api/models/apiModels/apiUpdateModels/updateUs
   styleUrl: './dialog-user.component.scss',
 })
 export class DialogUserComponent {
+  dialogRef = inject(MatDialogRef<DialogUserComponent, createUserApiModel>);
+  data = inject<clientModel>(MAT_DIALOG_DATA);
+
+  formbuilder = inject(FormBuilder);
+
+  formGroup = this.formbuilder.group({
+    firstName: new FormControl(this.data?.firstName ?? '', Validators.required),
+    lastName: new FormControl(this.data?.lastName ?? '', Validators.required),
+    email: new FormControl(this.data?.email ?? '', [
+      Validators.required,
+      Validators.email,
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.pattern(
+        new RegExp(
+          /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]*$/
+        )
+      ),
+    ]),
+    balance: new FormControl(this.data?.balance ?? 0, Validators.min(0)),
+    isRenting: new FormControl(this.data?.isRenting ?? false),
+  });
+
+  constructor() {
+    if (this.data) {
+      this.formGroup.controls.firstName.disable();
+      this.formGroup.controls.lastName.disable();
+      this.formGroup.controls.email.disable();
+      this.formGroup.controls.password.clearValidators()
+      this.formGroup.controls.password.disable()
+    }
+  }
+
+  closeDialog() {
+    this.dialogRef.close();
+  }
+
+  createUser() {
+    if (this.formGroup.valid) {
+      const formValues = this.formGroup.value;
+
+      let user: createUserApiModel = {
+        firstName: formValues.firstName || '',
+        lastName: formValues.lastName || '',
+        email: formValues.email || '',
+        password: formValues.password || '',
+        balance: formValues.balance || 0,
+        isRenting: formValues.isRenting || false,
+      };
+
+      this.dialogRef.close(user);
+    }
+  }
+  updateUser() {
+    if(this.formGroup.valid) {
+      const formValues = this.formGroup.value;
+
+      let user: updateUserApiModel = {
+        balance: formValues.balance || 0,
+        isRenting: formValues.isRenting || false
+      }
+
+      this.dialogRef.close(user);
+    }
+  }
 }
